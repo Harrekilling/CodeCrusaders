f19bfda334b48cd94fa084e9969785f254773fd8
SANNESPC\Sanne <sharrekilde@gmail.com>
2023-05-17 12:24:59 +0200

    Upload

---
+++ PigegruppenProjektSynsPunkt/ButiksSystem/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButiksSystem.Models
+{
+    internal class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+        public int CategoryID { get; set; }
+
+        public Product (int productID, string productName, decimal productPrice, int quantity, int categoryID)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            ProductPrice = productPrice;
+            Quantity = quantity;
+            CategoryID = categoryID;
+        }
+    }
+}
