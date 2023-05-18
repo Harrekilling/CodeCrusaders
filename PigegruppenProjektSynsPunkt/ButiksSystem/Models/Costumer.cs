2d65d749d82a7cb8987c5891e4a40d379be51be9
Emeli Kissov <eiek52989@edu.ucl.dk>
2023-05-18 13:51:44 +0200

    Updated the Costumer Model in "Butikssystem", new props Created and designed winforms for "CreateCostumer", "CreateProduct"

--- PigegruppenProjektSynsPunkt/ButiksSystem/Models/Costumer.cs
+++ PigegruppenProjektSynsPunkt/ButiksSystem/Models/Costumer.cs
@@ -9,5 +9,25 @@ namespace ButiksSystem.Models
     internal class Costumer
     {
         public int CostumerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Adress { get; set; }
+        public int PostalCode { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+
+
+        public Costumer(string firstName, 
+            string lastName, string phoneNumber, string adress, int postalCode, string city, string email) 
+        { 
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.PhoneNumber = phoneNumber;
+            this.Adress = adress;
+            this.PostalCode = postalCode;
+            this.City = city;
+            this.Email = email;
+        }
     }
 }
